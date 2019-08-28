931329481772bcfc918426e9b2eca113bf58da2f
deopurkara <deopurkara@gmail.com>
2019-08-28 23:40:09 +0530

    Updates

---
+++ precache-manifest.409617fa16f27084dfcec739a1d26039.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73275455dcdaba6519078f8e02948fc6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "29440d76a7e714fbc0d3",
+    "url": "/monster-rolodex/static/css/main.f92d5765.chunk.css"
+  },
+  {
+    "revision": "c9f0ff8d4457f60fa73e",
+    "url": "/monster-rolodex/static/js/2.55a63239.chunk.js"
+  },
+  {
+    "revision": "29440d76a7e714fbc0d3",
+    "url": "/monster-rolodex/static/js/main.c56c8288.chunk.js"
+  },
+  {
+    "revision": "1468997d5a4d4a4f0d08",
+    "url": "/monster-rolodex/static/js/runtime~main.174bb6fa.js"
+  }
+]);
